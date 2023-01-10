fc78339d0e5468b00f96508bad6b5aedc9f38aba
gstv1478 <gstv1478@gmail.com>
2023-01-10 15:14:57 -0300

    quarto commit

--- app.js
+++ app.js
@@ -46,4 +46,35 @@ const reviews = [
   const nextBtn = document.querySelector(".next-btn");
   const randomBtn = document.querySelector(".random-btn");
 
-  let currentItem = 0;
+  let currentItem = 3;
+
+  window.addEventListener("DOMContentLoaded", function () {
+    showPerson(currentItem);
+  });
+
+  function showPerson(person) {
+    const item = reviews[person];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+  }
+
+  nextBtn.addEventListener("click", function(){
+    currentItem++
+    if(currentItem > reviews.length - 1) {
+      currentItem = 0;
+    }   
+    showPerson(currentItem)
+  })
+  prevBtn.addEventListener("click", function(){
+    currentItem--
+    if(currentItem < 0) {
+      currentItem = reviews.length - 1;
+    }   
+    showPerson(currentItem)
+  })
+  randomBtn.addEventListener("click", function(){
+    currentItem = (Math.floor(Math.random()*reviews.length));
+    showPerson(currentItem)
+  })
